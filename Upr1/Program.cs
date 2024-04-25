29875a20a5ca4f8b2a1dc9563867700ecb48ccc6
Kostadin12 <kostadin13@abg.bg>
2024-04-25 21:58:52 +0300

    first init

---
+++ Upr1/Program.cs
@@ -0,0 +1,27 @@
+﻿using Upr1.Model;
+using Upr1.Others;
+using Upr1.View;
+using Upr1.ViewModel;
+
+class Program
+{
+    public static void Main()
+    {
+        User user = new User()
+        {
+            Name = "Mitko",
+            FakNum = "121221",
+            Mail = "@gg.gg",
+            Password = "123",
+            Role = UserRolesEnum.STUDENT
+        };
+
+        UserViewModel uvm = new UserViewModel(user);
+        UserView uv = new UserView(uvm);
+
+        uv.Display();
+        uv.DisplayColor();
+
+        Console.ReadKey();
+    }
+}
